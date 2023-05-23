380125895775c4eba1f176ab3b4e9eceb0c2b9f4
KimKihyeon <kidone0033@gmail.com>
2023-05-23 13:53:03 +0900

    좌표 버튼 만듦, 버튼 누를 시 버튼의 클래스 변화, 버튼의 클래스마다 좌표 구하는 기능 부여 및 회수

--- public/cesium.js
+++ public/cesium.js
@@ -41,28 +41,55 @@ $(window).on('load', function() {
         },
     });
 
+
     const handler = new Cesium.ScreenSpaceEventHandler(viewer.canvas);
-    handler.setInputAction((click) => {
-        let cartesian = viewer.camera.pickEllipsoid(click.position, viewer.scene.globe.ellipsoid);
-        if (cartesian) {
-            let cartographic = Cesium.Cartographic.fromCartesian(cartesian);
-            let longitude = Cesium.Math.toDegrees(cartographic.longitude);
-            let latitude = Cesium.Math.toDegrees(cartographic.latitude);
-            let height = cartographic.height;
-
-            console.log("클릭한 위치의 경도 : ", longitude);
-            console.log("클릭한 위치의 위도 : ", latitude);
-            console.log("클릭한 위치의 고도 : ", height);
+    
+    // 클릭한 캔버스에 좌표 찍는 함수
+    const getPosition = () => {
+        let clickHandler;
+        if($("#getPosition").hasClass('active')) {
+            clickHandler = handler.setInputAction((click) => {
+                let cartesian = viewer.camera.pickEllipsoid(click.position, viewer.scene.globe.ellipsoid);
+                if (cartesian) {
+                    let cartographic = Cesium.Cartographic.fromCartesian(cartesian);
+                    let longitude = Cesium.Math.toDegrees(cartographic.longitude);
+                    let latitude = Cesium.Math.toDegrees(cartographic.latitude);
+                    let height = cartographic.height;
+        
+                    console.log("클릭한 위치의 경도 : ", longitude);
+                    console.log("클릭한 위치의 위도 : ", latitude);
+                    console.log("클릭한 위치의 고도 : ", height);
+                    console.log("=========================");
+                    
+                }
+            }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+        } else {
+            handler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_CLICK, clickHandler);
         }
-    }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+    }
 
+    $("#getPosition").click(function(){
+        if($(this).hasClass('active')) {
+            $(this).removeClass('active');
+        } else {
+            $(this).addClass('active');
+        }
+        getPosition();
+    })
 
     $("#inputModel").click(() => {
+        let longitude = 126.8730332224969;
+        let latitude = 37.573589810697825;
+        let height = 9.313225746154785e-10;
+        // 위도 경도 높이를 Cartesian3로 바꿔줌
+        let cartographic = Cesium.Cartographic.fromDegrees(longitude, latitude, height);
+        let cartesian = Cesium.Ellipsoid.WGS84.cartographicToCartesian(cartographic);
+
         let box = viewer.entities.add({
             name : "Box",
-            position : Cesium.Cartesian3.fromDegrees(-3038479.495072682, 4048033.3357115067, 3868207.5514546167),
+            position : cartesian,
             box : {
-                dimensions :  new Cesium.Cartesian3(500.0, 500.0, 500.0),
+                dimensions :  new Cesium.Cartesian3(50.0, 50.0, 50.0),
                 material : Cesium.Color.RED,
                 outline : true,
                 outlinecolor : Cesium.Color.WHITE,
